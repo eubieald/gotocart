8d7ce7011d5f3d27ecdc3f279e7cdc1427dc52cd
eubieald <eubiealuad@gmail.com>
2023-12-02 22:38:55 +0800

    ADDED [added firebase authentication, logic for saving data to firebase db, rendering saved items in a ul element]

--- README.md
+++ README.md
@@ -7,18 +7,7 @@ This project is an example that was built along with a Youtube series however I 
 
 # LESSONS I LEARNED IN THIS PROJECT:
 * How to setup Firebase: https://console.firebase.google.com/?pli=1
-* 
-
-
-<!-- # HOW TO INSTALL
-1. Download the project in your local
-2. Open Chrome browser
-3. Type in the url: chrome://extensions/
-4. Enable Developer Mode in the Chrome Extension page (you can find this in the upper right most part within the page)
-5. Click the "Load unpacked" button (This will open your My Computer Files directory for Windows or Finder for Mac users)
-6. Choose the project that you just downloaded to unpacked
-7. Click the puzzle icon and open the extension named Eubie's URL Tracker -->
-
+* Security Rules: 
 
 
 ## Screenshots:
--- index.js
+++ index.js
@@ -1,26 +1,58 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, push, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+// Imports
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js';
+import {getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js';
 
-const appSettings = {
-    databaseURL: "https://gotocart-mobile-app-default-rtdb.asia-southeast1.firebasedatabase.app/"
-}
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+  apiKey: "AIzaSyAgFIMRpqBoYZ2vJoZ4st9OWMnapl2Ec3A",
+  authDomain: "gotocart-mobile-app.firebaseapp.com",
+  databaseURL: "https://gotocart-mobile-app-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gotocart-mobile-app",
+  storageBucket: "gotocart-mobile-app.appspot.com",
+  messagingSenderId: "292377266182",
+  appId: "1:292377266182:web:04f596d8420f3903311b87"
+};
 
-const app = initializeApp(appSettings),
-database = getDatabase(app),
-productsInDB = ref(database, 'products');
-
-console.log(database);
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+//  Initialize DB
+ const database = getDatabase(app);
+// Initialize ProductsInDB
+const shoppingListInDB = ref(database, 'shoppingList');
 
 const formEl = document.getElementById('form-el'),
-inputEl = document.getElementById('input-el');
+inputEl = document.getElementById('input-el'),
+ulEl = document.getElementById('ul-el');
+
+onValue(shoppingListInDB, (snapshot) => {
+  let itemsArray = [];
+  snapshot.forEach((item) => {
+    itemsArray.push(item.val());
+  });
+  renderList(itemsArray);
+});
+
+function renderList(items) {
+  let listItems = '';
+  items.forEach((item) => {
+    listItems += `<li>${item}</li>`;
+  });
+  ulEl.innerHTML = listItems;
+}
 
 // Event Listeners
 formEl.addEventListener('submit', handleForm);
 
-function handleForm(event) {
+function handleForm() {
   event.preventDefault();
-  const product = document.getElementById('input-el').value;
-  push(ref(productsInDB, 'products'), {
-    name: product
-  });
+
+  const item = inputEl.value;
+
+  if (item === '') {
+    alert('Please enter an item');
+    return;
+  }
+
+  push(shoppingListInDB, item);
+  inputEl.value = '';
 }
